511c4c2d773861f5a27f5ce5d079c4fe57ad9ea4
Viktoria804 <re25m020@technikum-wien.at>
2025-11-21 17:16:06 +0100

    Update main.cpp
    bessere struktur

--- main.cpp
+++ main.cpp
@@ -5,143 +5,170 @@
 #include <fstream>
 #include <thread>
 #include <string>
+#include <mutex>
 
 #pragma comment(lib, "ws2_32.lib")
 
-// Ein Worker, der zu einem Port connected und die JSON-Daten sammelt
-void readFromPort(const char* ip, int port, const char* outputFile)
-{
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (sock == INVALID_SOCKET)
-    {
-        std::cerr << "Socket creation failed for port " << port << "\n";
-        return;
+class JsonStreamReader {
+protected:
+    std::string ip;
+    int port;
+    std::string outputFile;
+
+public:
+    JsonStreamReader(const std::string& ip, int port, const std::string& out)
+        : ip(ip), port(port), outputFile(out) {}
+
+    virtual ~JsonStreamReader() {}
+
+    void operator()() {
+        run();
     }
 
-    sockaddr_in addr{};
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = inet_addr(ip);
+protected:
+    virtual void run() {
+        SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (sock == INVALID_SOCKET) {
+            std::cerr << "[Port " << port << "] Socket creation failed\n";
+            return;
+        }
 
-    std::cout << "[Port " << port << "] Verbinde...\n";
+        sockaddr_in addr{};
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        addr.sin_addr.s_addr = inet_addr(ip.c_str());
 
-    if (connect(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR)
-    {
-        std::cerr << "[Port " << port << "] Connect failed\n";
-        closesocket(sock);
-        return;
-    }
+        std::cout << "[Port " << port << "] Verbinde...\n";
 
-    std::cout << "[Port " << port << "] Verbunden! Sammle Daten...\n";
+        if (connect(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+            std::cerr << "[Port " << port << "] Connect failed\n";
+            closesocket(sock);
+            return;
+        }
 
-    std::ofstream jsonFile(outputFile);
-    if (!jsonFile)
-    {
-        std::cerr << "[Port " << port << "] Konnte Datei nicht öffnen.\n";
-        closesocket(sock);
-        return;
-    }
+        std::cout << "[Port " << port << "] Verbunden! Sammle JSON...\n";
 
-    jsonFile << "[\n";
+        std::ofstream jsonFile(outputFile);
+        if (!jsonFile) {
+            std::cerr << "[Port " << port << "] Konnte Datei nicht öffnen.\n";
+            closesocket(sock);
+            return;
+        }
 
-    const std::string START_MARKER = "---START---";
-    const std::string END_MARKER = "___END___";
+        jsonFile << "[\n";
 
-    std::string bufferAccum;
-    char recvBuf[4096];
-    bool first = true;
+        const std::string START_MARKER = "---START---";
+        const std::string END_MARKER = "___END___";
 
-    int maxMessages = 50;
-    int msgCount = 0;
+        std::string bufferAccum;
+        char recvBuf[4096];
+        bool first = true;
 
-    while (msgCount < maxMessages)
-    {
-        int len = recv(sock, recvBuf, sizeof(recvBuf) - 1, 0);
-        if (len > 0)
-        {
-            recvBuf[len] = '\0';
-            bufferAccum += recvBuf;
+        int maxMessages = 50;
+        int msgCount = 0;
 
-            while (true)
-            {
-                std::size_t startPos = bufferAccum.find(START_MARKER);
-                if (startPos == std::string::npos)
-                    break;
+        while (msgCount < maxMessages) {
+            int len = recv(sock, recvBuf, sizeof(recvBuf) - 1, 0);
+            if (len > 0) {
+                recvBuf[len] = '\0';
+                bufferAccum += recvBuf;
 
-                std::size_t endPos = bufferAccum.find(END_MARKER, startPos);
-                if (endPos == std::string::npos)
-                    break;
+                while (true) {
+                    std::size_t startPos = bufferAccum.find(START_MARKER);
+                    if (startPos == std::string::npos)
+                        break;
 
-                std::size_t jsonStart = startPos + START_MARKER.size();
-                std::size_t jsonLen = endPos - jsonStart;
+                    std::size_t endPos = bufferAccum.find(END_MARKER, startPos);
+                    if (endPos == std::string::npos)
+                        break;
 
-                std::string jsonSnippet = bufferAccum.substr(jsonStart, jsonLen);
+                    std::size_t jsonStart = startPos + START_MARKER.size();
+                    std::size_t jsonLen = endPos - jsonStart;
 
-                bufferAccum.erase(0, endPos + END_MARKER.size());
+                    std::string jsonSnippet =
+                        bufferAccum.substr(jsonStart, jsonLen);
 
-                while (!jsonSnippet.empty() &&
-                      (jsonSnippet.front() == '\n' || jsonSnippet.front() == '\r' || jsonSnippet.front() == ' '))
-                {
-                    jsonSnippet.erase(jsonSnippet.begin());
+                    bufferAccum.erase(0, endPos + END_MARKER.size());
+
+                    while (!jsonSnippet.empty() &&
+                        (jsonSnippet.front() == '\n' || jsonSnippet.front() == '\r' || jsonSnippet.front() == ' '))
+                        jsonSnippet.erase(jsonSnippet.begin());
+
+                    while (!jsonSnippet.empty() &&
+                        (jsonSnippet.back() == '\n' || jsonSnippet.back() == '\r' || jsonSnippet.back() == ' '))
+                        jsonSnippet.pop_back();
+
+                    if (!first)
+                        jsonFile << ",\n";
+                    first = false;
+
+                    jsonFile << jsonSnippet;
+
+                    msgCount++;
+                    std::cout << "[Port " << port << "] Message "
+                              << msgCount << " gespeichert.\n";
+
+                    if (msgCount >= maxMessages)
+                        break;
                 }
-                while (!jsonSnippet.empty() &&
-                      (jsonSnippet.back() == '\n' || jsonSnippet.back() == '\r' || jsonSnippet.back() == ' '))
-                {
-                    jsonSnippet.pop_back();
-                }
-
-                if (!first)
-                    jsonFile << ",\n";
-                first = false;
-
-                jsonFile << jsonSnippet;
-
-                msgCount++;
-                std::cout << "[Port " << port << "] Message " << msgCount << " gespeichert.\n";
-
-                if (msgCount >= maxMessages)
-                    break;
+            }
+            else if (len == 0) {
+                std::cout << "[Port " << port << "] Verbindung geschlossen.\n";
+                break;
+            }
+            else {
+                std::cerr << "[Port " << port << "] recv Fehler\n";
+                break;
             }
         }
-        else if (len == 0)
-        {
-            std::cout << "[Port " << port << "] Verbindung geschlossen.\n";
-            break;
-        }
-        else
-        {
-            std::cerr << "[Port " << port << "] recv Fehler\n";
-            break;
-        }
+
+        jsonFile << "\n]\n";
+        jsonFile.close();
+
+        closesocket(sock);
+        std::cout << "[Port " << port << "] Fertig! JSON gespeichert.\n";
     }
+};
 
-    jsonFile << "\n]\n";
-    jsonFile.close();
 
-    closesocket(sock);
+// -----------------------------------------------------------
+// Spezifische Klassen für Laser & Odom (nur Port + Dateiname)
+// -----------------------------------------------------------
 
-    std::cout << "[Port " << port << "] Fertig! "
-              << msgCount << " Messages in '" << outputFile << "' gespeichert.\n";
-}
+class LaserReader : public JsonStreamReader {
+public:
+    LaserReader(const std::string& ip)
+        : JsonStreamReader(ip, 9997, "laser_output.json") {}
+};
 
-int main()
-{
-    // Winsock starten
+class OdomReader : public JsonStreamReader {
+public:
+    OdomReader(const std::string& ip)
+        : JsonStreamReader(ip, 9998, "odom_output.json") {}
+};
+
+
+// -----------------------------------------------------------
+// Main – startet beide Reader parallel
+// -----------------------------------------------------------
+
+int main() {
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    const char* ip = "192.168.100.52";
+    const std::string ip = "192.168.100.52";
 
-    // Zwei parallele Threads für Laser + Odom
-    std::thread tLaser(readFromPort, ip, 9997, "laser_output.json");
-    std::thread tOdom(readFromPort, ip, 9998, "odom_output.json");
+    LaserReader laser(ip);
+    OdomReader odom(ip);
 
-    // Warten, bis beide Threads fertig sind
+    std::thread tLaser(std::ref(laser));
+    std::thread tOdom(std::ref(odom));
+
     tLaser.join();
     tOdom.join();
 
     WSACleanup();
 
-    std::cout << "Alle Verbindungen abgeschlossen.\n";
+    std::cout << "Alle Verbindungen beendet.\n";
     return 0;
 }
